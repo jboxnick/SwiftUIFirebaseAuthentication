defbc18e338b7b9a59596a31957e3101744cec50
jboxnick <j.boxnick@web.de>
2021-12-29 13:39:55 +0100

    Initial Commit

---
+++ SwiftUIFirebaseAuthentication/SwiftUIFirebaseAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseAuthenticationApp.swift
+//  SwiftUIFirebaseAuthentication
+//
+//  Created by Julian on 29.12.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
